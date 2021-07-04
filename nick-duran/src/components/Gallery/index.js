b727858937610ad5c54109356b24f1c19f0e4227
nvduran <nvduran@gmail.com>
2021-07-03 21:33:55 -0500

    carousel cleaning

--- nick-duran/src/components/Gallery/index.js
+++ nick-duran/src/components/Gallery/index.js
@@ -22,8 +22,8 @@ function Gallery(props) {
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3></h3>
+              <p></p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -34,8 +34,8 @@ function Gallery(props) {
             />
 
             <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3></h3>
+              <p></p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -46,8 +46,8 @@ function Gallery(props) {
             />
 
             <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              <h3></h3>
+              <p></p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel></Col>
@@ -61,8 +61,8 @@ function Gallery(props) {
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3></h3>
+              <p></p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -73,8 +73,8 @@ function Gallery(props) {
             />
 
             <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3></h3>
+              <p></p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel></Col>
